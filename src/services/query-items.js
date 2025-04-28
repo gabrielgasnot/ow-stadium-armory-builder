937bc1d69440d96d7217b684cf62f7207a880254
Antoine Lemaire <antoine@bouffe.io>
2025-04-28 14:02:53 +0200

    Use 'common' instead of 'normal' (#25)

--- src/services/query-items.js
+++ src/services/query-items.js
@@ -1,7 +1,7 @@
 import { basicItems } from "../db/db";
 
 const getAllItemsByHero = (hero) => {
-    return [...hero?.items.normal, ...hero?.items.rare, ...hero?.items.epic, ...basicItems.normal, ...basicItems.rare, ...basicItems.epic];
+    return [...hero?.items.common, ...hero?.items.rare, ...hero?.items.epic, ...basicItems.common, ...basicItems.rare, ...basicItems.epic];
 }
 
 export default getAllItemsByHero;
