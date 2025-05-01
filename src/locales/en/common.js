bf025a14ae2ce3c115420e32c74d2d6390f7ba36
GGA <gabriel.gasnot@gmail.com>
2025-05-01 07:28:21 +0200

    i18n draft

---
+++ src/locales/en/common.js
@@ -0,0 +1,15 @@
+const commonTranslations = {
+  pickHero: "Select a hero to start your build",
+  importBuild: "Import a build",
+  returnHeroes: "Return to heroes",
+  powers: "Powers",
+  items: "Items",
+  cost: "Build cost:",
+  share: "Share",
+  capture: "Capture",
+  round: "Round",
+  item: "Item",
+  
+};
+
+export default commonTranslations;
