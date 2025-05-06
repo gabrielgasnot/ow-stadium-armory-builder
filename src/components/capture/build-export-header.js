3a5bee476545ac08616d2b5d62be551eee48442e
GGA <gabriel.gasnot@gmail.com>
2025-05-06 13:58:07 +0200

    review capture header

--- src/components/capture/build-export-header.js
+++ src/components/capture/build-export-header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, Paper, Divider, alpha } from "@mui/material";
+import { Box, Typography, Paper } from "@mui/material";
 
 function BuildExportHeader({ hero, shareLink }) {
   return (
@@ -9,67 +9,25 @@ function BuildExportHeader({ hero, shareLink }) {
         paddingLeft: 2,
         paddingRight: 2,
         display: "flex",
-        flexGrow: 1,
         flexDirection: "column",
         gap: 2,
-        width: "600px",
+        width: "550px",
+        p: 2,
       }}
     >
-      {/* Header */}
-      <Box
-        sx={{
-          display: "flex",
-          width: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "row",
-          gap: 2,
-        }}
-      >
-        <Box
-          component="img"
-          src={`${process.env.PUBLIC_URL}/logo-light.png`}
-          alt="Overwatch Builds"
-          sx={{
-            width: 83,
-            height: 64,
-            objectFit: "contain",
-          }}
-        />
-        <Typography
-          variant="h4"
-          sx={{ textAlign: "center", alignContent: "baseline" }}
-        >
-          Overwatchbuilds.com
-        </Typography>
-      </Box>
-      <Divider
-        variant="middle"
-        sx={(theme) => ({
-          backgroundColor: alpha(theme.palette.custom.orange, 0.1),
-          height: "1px",
-          boxShadow: "0px 2px 4px rgba(249, 158, 26, 0.7)",
-          borderRadius: "1px",
-          mt: 2,
-          mb: 2,
-        })}
-      />
-      {/* Content */}
       <Box
         sx={{
           display: "flex",
           width: "100%",
           justifyContent: "space-between",
-          alignItems: "center",
           flexDirection: "row",
           gap: 2,
         }}
       >
-        {/* Left image section - hidden on xs */}
         <Box
           sx={{
-            width: 100,
-            height: 100,
+            width: 125,
+            height: 125,
             backgroundColor: "background.paper",
             borderRadius: 1,
             backgroundImage: `url(${process.env.PUBLIC_URL}/heroes/${hero.id}.png)`,
@@ -86,7 +44,7 @@ function BuildExportHeader({ hero, shareLink }) {
             flexGrow: 1,
             display: "flex",
             flexDirection: "column",
-            justifyContent: "flex-start",
+            justifyContent: "space-between",
           }}
         >
           {/* Name + Icon */}
@@ -112,14 +70,32 @@ function BuildExportHeader({ hero, shareLink }) {
               {hero.name}
             </Typography>
           </Box>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            Build created{" "}
-            {`${new Date().getFullYear()}-${String(
-              new Date().getMonth() + 1
-            ).padStart(2, "0")}-${String(new Date().getDate()).padStart(
-              2,
-              "0"
-            )}`}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "baseline",
+              flexDirection: "column",
+            }}
+          >
+            Build created on
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+              <Box
+                component="img"
+                src={`${process.env.PUBLIC_URL}/logo-light.png`}
+                alt="Overwatch Builds"
+                sx={{
+                  width: 42,
+                  height: 32,
+                  objectFit: "contain",
+                }}
+              />
+              <Typography
+                variant="h4"
+                sx={{ textAlign: "left", alignContent: "baseline" }}
+              >
+                Overwatchbuilds.com
+              </Typography>
+            </Box>
           </Box>
         </Box>
       </Box>
