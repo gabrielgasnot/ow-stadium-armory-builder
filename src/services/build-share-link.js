96311dd88f49b8eb5a84e5573f4bd280f0439f2c
GGASTM <gabriel.gasnot@st.com>
2025-05-13 10:55:15 +0200

    fix export error

--- src/services/build-share-link.js
+++
@@ -1,5 +0,0 @@
-export default function buildShareLink(encodedBuild) {
-  let baseUrl = window.location.origin + window.location.pathname;
-  if (!baseUrl.endsWith("/")) baseUrl += "/";
-  return `${baseUrl}#/${encodedBuild}`;
-}
--- src/services/export-build.ts
+++ src/services/export-build.ts
@@ -1,15 +1,17 @@
 import { compressString } from "../helpers/compression.js";
 import { Hero } from "../models/hero.js";
+import { Perk } from "../models/perk.js";
 import { Round } from "../models/round.js";
 
 const exportBuild = (hero: Hero, rounds: Round[]) => {
   const heroId = hero.id;
   const roundsDatas = rounds.map((round) =>
-    [...round.powers, ...round.items].join("-")
+    [
+      ...round.powers.map((power: Perk) => power.id),
+      ...round.items.map((item: Perk) => item.id),
+    ].join("-")
   );
-
   const buildId = `${heroId}|${roundsDatas.join("|")}`;
-
   return compressString(buildId);
 };
 
