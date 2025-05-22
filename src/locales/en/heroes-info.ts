865da8ebb7735034a532f90e8758451e67b9cb4f
GGASTM <gabriel.gasnot@st.com>
2025-05-22 18:00:21 +0200

    add heroes skills

---
+++ src/locales/en/heroes-info.ts
@@ -0,0 +1,434 @@
+export default {
+  "5I001": {
+    name: "Rocket Hammer",
+    primary: "Devastating melee weapon.",
+  },
+  "5I002": {
+    name: "Charge",
+    primary: "Charge forward and smash an enemy against a wall.",
+  },
+  "5I003": {
+    name: "Fire Strike",
+    primary: "Launch a fiery projectile.",
+  },
+  "5I004": {
+    name: "Barrier Field",
+    primary: "Deploy a frontal energy barrier.",
+  },
+  "5I005": {
+    name: "Earthshatter",
+    primary: "Knock down all enemies in front of you.",
+  },
+  "2I001": {
+    name: "Hellfire Shotguns",
+    primary: "Short range spread weapons.",
+  },
+  "2I002": {
+    name: "Shadow Step",
+    primary: "Teleport to a targeted location.",
+    notboosted: true,
+  },
+  "2I003": {
+    name: "Wraith Form",
+    primary: "Move faster and become invulnerable, but you cannot shoot.",
+    notboosted: true,
+  },
+  "2I004": {
+    name: "Death Blossom",
+    primary: "Damage all nearby enemies.",
+  },
+  "2I005": {
+    name: "The Reaping",
+    primary: "Dealing damage heals you.",
+  },
+  "15I001": {
+    name: "Heavy Pulse Rifle",
+    primary: "Automatic assault weapon.",
+  },
+  "15I002": {
+    name: "Sprint",
+    primary: "Run faster while moving forward.",
+    notboosted: true,
+  },
+  "15I003": {
+    name: "Biotic Field",
+    primary: "Deploy a field that heals you and your allies.",
+  },
+  "15I004": {
+    name: "Helix Rocket",
+    primary: "Launch a volley of explosive rockets.",
+  },
+  "15I005": {
+    name: "Tactical Visor",
+    primary: "Automatically aims your weapon at targets in view.",
+  },
+  "6I001": {
+    name: "Caduceus Staff",
+    primary: "Heal an ally.",
+    secondary: "Increase an ally's damage inflicted.",
+  },
+  "6I002": {
+    name: "Caduceus Blaster",
+    primary: "Automatic weapon.",
+  },
+  "6I003": {
+    name: "Guardian Angel",
+    primary:
+      "Fly towards an ally. While in flight, Jump launches you forward, and Crouch launches you upward.",
+  },
+  "6I004": {
+    name: "Resurrect",
+    primary: "Revive a dead teammate.",
+  },
+  "6I005": {
+    name: "Valkyrie",
+    primary: "Gain the ability to fly. Abilities are enhanced.",
+  },
+  "6I006": {
+    name: "Angelic Descent",
+    primary: "Fall very slowly.",
+  },
+  "6I007": {
+    name: "Symphatic Recovery",
+    primary: "Healing allies with the Caduceus Staff heals you.",
+  },
+  "26I001": {
+    name: "Biotic Grasp",
+    primary: "Heal all allies in front of you. Consumes biotic enery.",
+    secondary:
+      "Long range beam weapon. Damage dealt heals you and resplenishes your biotic energy.",
+  },
+  "26I002": {
+    name: "Biotic Orb",
+    primary:
+      "Launch a bouncing sphere that either heals nearby allies or damage nearby enemies.",
+  },
+  "26I003": {
+    name: "Fade",
+    primary:
+      "Disappear, move faster, and become invulnerable, but you cannot shoot.",
+  },
+  "26I004": {
+    name: "Coalescence",
+    primary: "Fire a beam that heals allies and damages enemies.",
+  },
+  "19I001": {
+    name: "Fusion Cannons",
+    primary: "Automatic short-range spread weapons.",
+  },
+  "19I002": {
+    name: "Light Gun",
+    primary: "Automatic weapon.",
+  },
+  "19I003": {
+    name: "Boosters",
+    primary: "Fly in the direction you are facing.",
+  },
+  "19I004": {
+    name: "Defense Matrix",
+    primary: "Block projectiles in an area in front of you.",
+  },
+  "19I005": {
+    name: "Micro Missiles",
+    primary: "Launch a volley of explosive rockets.",
+  },
+  "19I006": {
+    name: "Self-Destruct",
+    primary:
+      "Eject and overload your mech, causing it to explode after a short time.",
+  },
+  "19I007": {
+    name: "Call Mech",
+    primary: "Call down a new mech.",
+  },
+  "19I008": {
+    name: "Eject!",
+    primary: "Eject out of your mech when it is destroyed.",
+  },
+  "21I001": {
+    name: "Shuriken",
+    primary: "Throw an accurate burst of 3 projectiles.",
+    secondary: "Throw a fan of 3 projectiles in an arc.",
+  },
+  "21I002": {
+    name: "Deflect",
+    primary:
+      "Deflect incoming projectiles towards the direction you are aiming and block melee attacks.",
+    notboosted: true,
+  },
+  "21I003": {
+    name: "Swift Strike",
+    primary:
+      "Rapidly dash forward and inflict damage on enemies. Eliminations reset the cooldown.",
+  },
+  "21I004": {
+    name: "Dragondblade",
+    primary: "Unsheathe a deadly melee weapon.",
+  },
+  "21I005": {
+    name: "Cyber-Agility",
+    primary: "Climb on walls and double jump.",
+    notboosted: true,
+  },
+  "24I001": {
+    name: "Augmented Fusion Driver",
+    primary: "Automatic heat based weapon.",
+  },
+  "24I002": {
+    name: "Energy Javelin",
+    primary:
+      "Launch your javelin at an emey to stun and knock them back. More effective if the enemy hits a wall.",
+  },
+  "24I003": {
+    name: "Fortify",
+    primary:
+      "Gain temporary health, reducing all damage taken and becoming unstoppable.",
+  },
+  "24I004": {
+    name: "Javelin Spin",
+    primary:
+      "Spin your javelin to destroy projectiles and block melee attacks, while also pushing enemies and increasing forward speed.",
+  },
+  "24I005": {
+    name: "Terra Surge",
+    primary:
+      "Sweep in enemies and anchor down, gaining the effects of Fortify and charging up a surge of damage. Fire to unleash the surge early.",
+  },
+  "34I001": {
+    name: "Scattergun",
+    primary: "Pump action shotgun.",
+  },
+  "34I002": {
+    name: "Jagged Blade",
+    primary:
+      "Active: Throw the blade. Re-activate to pull it back, along with any impaled enemy.",
+    secondary:
+      "Passive: Wound enemies hit by Quick Melee or Throw, dealing damage over time.",
+  },
+  "34I003": {
+    name: "Commanding Shout",
+    primary: "Gain temporary health and movement speed to yourself and allies.",
+  },
+  "34I004": {
+    name: "Carnage",
+    primary:
+      "Wound all enemies in front of you, dealing damage over time and reducing the cooldown for each enemy hit.",
+  },
+  "34I005": {
+    name: "Rampage",
+    primary:
+      "Charge forward. Wound enemies, dealing damage over time and preventing them from being healed.",
+  },
+  "34I006": {
+    name: "Adrelaline Rush",
+    primary: "Heal from all damage over time dealt by wounds.",
+  },
+  "13I001": {
+    name: "Particle Cannon",
+    primary: "Short-range linear beam weapon.",
+    secondary: "Energy grenade launcher.",
+  },
+  "13I002": {
+    name: "Particle Barrier",
+    primary: "Create a damage barrier around you.",
+  },
+  "13I003": {
+    name: "Projected Barrier",
+    primary: "Create a damage barrier around an ally.",
+  },
+  "13I004": {
+    name: "Graviton Surge",
+    primary: "Launch a gravity well that pulls enemies to it.",
+  },
+  "13I005": {
+    name: "Energy",
+    primary: "Damage blocked by barriers increases Particle Cannon damage.",
+  },
+  "35I001": {
+    name: "Healing Ofuda",
+    primary:
+      "Channel a burst of healing talismans that can seek targeted allies.",
+  },
+  "35I002": {
+    name: "Kunai",
+    primary: "Thrown long-ranged projectile.",
+  },
+  "35I003": {
+    name: "Swift Step",
+    primary: "Teleport directly to an ally.",
+    notboosted: true,
+  },
+  "35I004": {
+    name: "Protection Suzu",
+    primary:
+      "Throw a protective charm to make allies become briefly invulnerable and cleansed of most negative effects.",
+  },
+  "35I005": {
+    name: "Kitsune Rush",
+    primary:
+      "Summon a fox spirit that rushes forward, accelerating the movement, attack speed, and cooldowns of allies that follow in its path.",
+    notboosted: true,
+  },
+  "35I006": {
+    name: "Wall Climb",
+    primary: "Jump at walls to climb up them.",
+  },
+  "22I001": {
+    name: "Biotic Rifle",
+    primary: "Long-range rifle that heals allies and damages enemies.",
+    secondary: "Hold to zoom in.",
+  },
+  "22I002": {
+    name: "Sleep Dart",
+    primary: "Fires a dart that puts an enemy to sleep.",
+  },
+  "22I003": {
+    name: "Biotic Grenade",
+    primary:
+      "Throws a grenade that heals and increases healing on allies, while damaging and preventing healing on enemies.",
+  },
+  "22I004": {
+    name: "Nano Boost",
+    primary: "Increases an ally's damage, while reducing damage taken.",
+  },
+  "14I001": {
+    name: "Peacekeeper",
+    primary: "Accurate, powerful revolver.",
+    secondary: "Wildly fire remaining rounds.",
+  },
+  "14I002": {
+    name: "Combat Roll",
+    primary:
+      "Roll in the direction you're moving to take reduced damage and reload.",
+    notboosted: true,
+  },
+  "14I003": {
+    name: "Flashbang",
+    primary:
+      "Hinder enemies in front of you, slowing them and disabling movement abilities.",
+  },
+  "14I004": {
+    name: "Deadeye",
+    primary: "Face off against your enemies. Fire to trigger the shots.",
+  },
+  "29I001": {
+    name: "The Viper",
+    primary: "Semi-automatic rifle.",
+    secondary:
+      "Hold to zoom in, increasing damage and accuracy but slowing rate of fire.",
+  },
+  "29I002": {
+    name: "Coach Gun",
+    primary: "Blast enemies in front of you and knock yourself backwards.",
+  },
+  "29I003": {
+    name: "Dynamite",
+    primary:
+      "Throw an explosive that detonates after a short delay or immediately when shot.",
+  },
+  "29I004": {
+    name: "B.O.B.",
+    primary:
+      "Deploy Bob. He charges forward and knocks enemies into the air, then attacks with his arm cannons.",
+  },
+  "41I001": {
+    name: "Mediblaster",
+    primary: "Burst fire weapon that heals allies and damages enemies.",
+  },
+  "41I002": {
+    name: "Pulsar Torpedoes",
+    primary:
+      "Lock on then press fire to trigger homing projectiles at locked targets. Heals allies over time and damages enemies.",
+  },
+  "41I003": {
+    name: "Glide Boost",
+    primary: "Glide horizontally with increased movement speed.",
+    notboosted: true,
+  },
+  "41I004": {
+    name: "Hyper Ring",
+    primary:
+      "Deploy a ring that increases movement speed of allies that pass through it.",
+    notboosted: true,
+  },
+  "41I005": {
+    name: "Orbital Ray",
+    primary:
+      "Call down a ray that travels forward, healing allies and increasing their damage.",
+  },
+  "41I006": {
+    name: "Martian Overboots",
+    primary: "While airborne, Jump to double Jump and hold Jump to hover.",
+  },
+  "20I001": {
+    name: "Endothermic Blaster",
+    primary: "Short-range spray weapon that slows enemies.",
+    secondary: "Long-range icicle launcher.",
+  },
+  "20I002": {
+    name: "Cryo-freeze",
+    primary: "Become invulnerable and heal yourself.",
+    notboosted: true,
+  },
+  "20I003": {
+    name: "Ice wall",
+    primary: "Create a wall in front of you.",
+    notboosted: true,
+  },
+  "20I004": {
+    name: "Blizzard",
+    primary:
+      "Launch a weather control drone that freezes enemies in a wide area.",
+  },
+  "16I001": {
+    name: "Sonic Amplifier",
+    primary: "Sonic projectile launcher.",
+  },
+  "16I002": {
+    name: "Crossfade",
+    primary:
+      "Switches between two songs: Healing Boost heals nearby allies, and Speed Boost makes nearby allies move faster.",
+  },
+  "16I003": {
+    name: "Amp It Up",
+    primary: "Increase the effectiveness of your current song.",
+  },
+  "16I004": {
+    name: "Soundwave",
+    primary: "Create a short-range blast wave to knock enemies away from you.",
+  },
+  "16I005": {
+    name: "Sound Barrier",
+    primary: "Create temporary extra health for nearby allies.",
+  },
+  "16I006": {
+    name: "Wall Ride",
+    primary: "Jump onto a wall to ride along it.",
+  },
+  "43I001": {
+    name: "Revdraw Crossbow",
+    primary: "Rapidly fire automatic bolts.",
+  },
+  "43I002": {
+    name: "Take Aim",
+    primary:
+      "Slow your momentum to take aim with a high speed explosive bolt. Using [Quick Dash] refreshes [Take Aim].",
+  },
+  "43I003": {
+    name: "Quick Dash",
+    primary: "Vault in the direction you're moving and refresh [Take Aim].",
+  },
+  "43I004": {
+    name: "Updraft",
+    primary: "Launch upward with a gust of wind.",
+  },
+  "43I005": {
+    name: "Bola Shot",
+    primary:
+      "Fire an explosive bola. Hitting an enemy wraps them up and pulls in other nearby enemies.",
+  },
+  "43I006": {
+    name: "Bounty Hunting",
+    primary: "Every 3 Final Blows, gain bonus ultimate charge.",
+  },
+};
---
+++ src/locales/fr/heroes-info.ts
@@ -0,0 +1,448 @@
+export default {
+  "5I001": {
+    name: "Marteau à réaction",
+    primary: "Arme de mêlée dévastatrice.",
+  },
+  "5I002": {
+    name: "Charge",
+    primary: "Vous foncez en avant et écrasez un ennemi contre un mur.",
+  },
+  "5I003": {
+    name: "Frappe de feu",
+    primary: "Vous lancez un projectile enflammé.",
+  },
+  "5I004": {
+    name: "Écran",
+    primary: "Déployer une barrière d'énergie frontale.",
+  },
+  "5I005": {
+    name: "Choc sismique",
+    primary: "Vous renversez tous les ennemis en face de vous.",
+  },
+  "2I001": {
+    name: "Pompes Funèbres",
+    primary: "Armes à dispersion de courte portée.",
+  },
+  "2I002": {
+    name: "Voie des ombres",
+    primary: "Vous vous téléportez à l'endroit ciblé.",
+    notboosted: true,
+  },
+  "2I003": {
+    name: "Forme spectrale",
+    primary:
+      "Vous vous déplacez plus rapidement et devenez invulnérable, mais vous ne pouvez pas tirer.",
+    notboosted: true,
+  },
+  "2I004": {
+    name: "Éclosion Mortelle",
+    primary: "Vous infligez des dégâts à tous les ennemis à proximité.",
+  },
+  "2I005": {
+    name: "La Moisson",
+    primary: "Infliger des dégâts vous rend des points de vie.",
+  },
+  "15I001": {
+    name: "Fusil à impulsion",
+    primary: "Arme d'assaut automatique.",
+  },
+  "15I002": {
+    name: "Sprint",
+    primary: "Vous vous déplacez plus vite vers l'avant.",
+    notboosted: true,
+  },
+  "15I003": {
+    name: "Champ biotique",
+    primary: "Vous déployez un champ de soins pour vous et vos alliés.",
+  },
+  "15I004": {
+    name: "Roquettes LX",
+    primary: "Lance une volée de roquettes explosives.",
+  },
+  "15I005": {
+    name: "Visière Tactique",
+    primary: "Votre arme vise automatiquement les cibles en vue.",
+  },
+  "6I001": {
+    name: "Caducée",
+    primary: "Soigne un personnage allié.",
+    secondary: "Augmente les dégâts infligés par un allié.",
+  },
+  "6I002": {
+    name: "Miséricorde",
+    primary: "Arme automatique.",
+  },
+  "6I003": {
+    name: "Ange Gardien",
+    primary:
+      "Volez jusqu'à un allié. En vol, sauter vous projette en avant, et s'accroupir vous projette en l'air.",
+  },
+  "6I004": {
+    name: "Résurrection",
+    primary: "Vous ressuscitez une cible alliée.",
+  },
+  "6I005": {
+    name: "Valkyrie",
+    primary: "Vous confère la capacité de voler. Améliore vos capacités.",
+  },
+  "6I006": {
+    name: "Saut de l'Ange",
+    primary: "La chute est considérablement ralentie.",
+  },
+  "6I007": {
+    name: "Guérison Favorable",
+    primary: "Soigner des alliés avec [Caducée] vous rend des points de vie.",
+  },
+  "26I001": {
+    name: "Emprise Biotique",
+    primary: "Soigne les alliés devant vous. Consomme de l'énergie biotique.",
+    secondary:
+      "Arme projetant un rayon à longue portée. Les dégâts infligés vous soignent et restaurent votre énergie biotique.",
+  },
+  "26I002": {
+    name: "Orbe Biotique",
+    primary:
+      "Vous lancez une sphère rebondissante qui soigne les membres de votre équipe à proximité ou inflige des dégâts aux adversaires à proximité.",
+  },
+  "26I003": {
+    name: "Volatilité",
+    primary:
+      "Vous disparaissez, vous vous déplacez plus rapidement et devenez invulnérable, mais ne pouvez pas tirer.",
+  },
+  "26I004": {
+    name: "Coalescence",
+    primary: "Libère un rayon qui soigne les alliés et blesse les ennemis.",
+  },
+  "19I001": {
+    name: "Fusio-Canons",
+    primary: "Armes automatiques à dispersion de courte portée.",
+  },
+  "19I002": {
+    name: "Pistolaser",
+    primary: "Arme automatique.",
+  },
+  "19I003": {
+    name: "Turboréacteurs",
+    primary: "Vous volez droit devant vous.",
+  },
+  "19I004": {
+    name: "Matrice Défensive",
+    primary: "Vous bloquez les projectiles dans une zone devant vous.",
+  },
+  "19I005": {
+    name: "Micro-Missiles",
+    primary: "Lance une volée de roquettes explosives.",
+  },
+  "19I006": {
+    name: "Autodestruction",
+    primary:
+      "Vous vous éjectez du méca en déclenchant une surcharge qui le fait exploser après un court délai.",
+  },
+  "19I007": {
+    name: "Appel du Méca",
+    primary: "Vous appelez un nouveau méca.",
+  },
+  "19I008": {
+    name: "Éjection !",
+    primary: "Vous vous éjectez du méca quand il est détruit.",
+  },
+  "21I001": {
+    name: "Shuriken",
+    primary: "Vous lancez 3 projectiles dans un tir concentré.",
+    secondary: "Vous lancez 3 projectiles en arc de cercle.",
+  },
+  "21I002": {
+    name: "Riposte",
+    primary:
+      "Vous déviez les projectiles dans la direction que vous visez et vous bloquez les attaques en mêlée.",
+    notboosted: true,
+  },
+  "21I003": {
+    name: "Frappe du Vent",
+    primary:
+      "Vous foncez devant vous et infligez des dégâts aux ennemis. Éliminer une cible réinitialise le temps de recharge.",
+  },
+  "21I004": {
+    name: "Lame du Dragon",
+    primary: "Dégaine une arme de mêlée mortelle.",
+  },
+  "21I005": {
+    name: "Agilité Cybernétique",
+    primary: "Capacité à escalader les murs et à effectuer un double saut.",
+    notboosted: true,
+  },
+  "24I001": {
+    name: "Canon à Fusion amélioré",
+    primary: "Arme automatique thermique.",
+  },
+  "24I002": {
+    name: "Javelot d'Énergie",
+    primary:
+      "Vous lancez votre javelot sur une cible, ce qui l'étourdit et la repousse. L'efficacité de la compétence est augmentée si la cible heurte un mur.",
+  },
+  "24I003": {
+    name: "Blindage",
+    primary:
+      "Vous gagnez des points de vie temporaires, vous subissez moins de dégâts et rien ne peut vous arrêter.",
+  },
+  "24I004": {
+    name: "Rotation de Javelot",
+    primary:
+      "Vous faites tourner votre javelot pour détruire les projectiles, bloquer les attaques en mêlée, repousser les adversaires et augmenter votre vitesse quand vous avancez.",
+  },
+  "24I005": {
+    name: "Vague Terra",
+    primary:
+      "Vous attirez les adveraires et vous vous immobilisez. Vous obtenez les effets de [Blindage] et vous chargez une vague de dégâts. Tirez pour relâcher la vague plus tôt.",
+  },
+  "34I001": {
+    name: "Fusil à dispersion",
+    primary: "Fusil à pompe.",
+  },
+  "34I002": {
+    name: "Lame Crantée",
+    primary:
+      "Effet actif: vous lancez la lame. Réactivez la compétence pour la faire revenir avec la cible qui y est potentiellement impalée.",
+    secondary:
+      "Effet passif: les victimes d'un coup en mêlée ou d'un lancer sont blessées, ce qui inflige des dégâts sur la durée.",
+  },
+  "34I003": {
+    name: "Cri de Commandement",
+    primary:
+      "Vos points de vie et votre vitesse de déplacement augmentent temporairement. Cet effet s'applique également aux membres de votre équipe.",
+  },
+  "34I004": {
+    name: "Carnage",
+    primary:
+      "Vous blessez l'ensemble des adversaires devant vous, leur infligeant des dégâts sur la durée. Le temps de recharge est réduit pour chaque cible touchée.",
+  },
+  "34I005": {
+    name: "Massacre",
+    primary:
+      "Vous chargez en avant. Vous blessez les adversaires, leur infligeant des dégâts sur la durée et les empêchant de se soigner.",
+  },
+  "34I006": {
+    name: "Montée d'Adrenaline",
+    primary:
+      "Vous soignez tous les dégâts infligés sur la durée par les blessures.",
+  },
+  "13I001": {
+    name: "Canon à Particules",
+    primary: "Arme projetant un rayon linéaire à courte portée.",
+    secondary: "Lance-grenades à énergie.",
+  },
+  "13I002": {
+    name: "Écran de Particules",
+    primary: "Crée un écran absorbant les dégâts autour de vous.",
+  },
+  "13I003": {
+    name: "Écran Généré",
+    primary: "Crée un écran absorbant les dégâts autour d'un allié.",
+  },
+  "13I004": {
+    name: "Orbe à Gravitons",
+    primary: "Vous créez un puits de gravité qui attire les ennemis.",
+  },
+  "13I005": {
+    name: "Énergie",
+    primary:
+      "Les dégâts bloqués par les écrans augmentent les dégâts de [Canon à Particules].",
+  },
+  "35I001": {
+    name: "O-fuda de Guérison",
+    primary:
+      "Vous expédiez une salve de talismans de soins qui cherchent à atteindre des cibles alliées.",
+  },
+  "35I002": {
+    name: "Kunai",
+    primary: "Projectile lancé à longue portée.",
+  },
+  "35I003": {
+    name: "Pas Véloce",
+    primary: "Vous vous téléportez directement vers un ou une alliée.",
+    notboosted: true,
+  },
+  "35I004": {
+    name: "Suzu de Protection",
+    primary:
+      "Vous lancez un charme de protection qui soigne la plupart des effets négatifs des membres de votre équipe et les rend brièvement invulnérables.",
+  },
+  "35I005": {
+    name: "Ruée de Kitsune",
+    primary:
+      "Vous invoquez un esprit vulpin qui se précipite en avant, accélérant la vitesse de déplacement. La vitesse d'attaque et les temps de recharge des personnages alliés qui se trouvent dans son sillage.",
+    notboosted: true,
+  },
+  "35I006": {
+    name: "Escalade",
+    primary: "Vous sautez sur les murs pour les escalader.",
+  },
+  "22I001": {
+    name: "Fusil Biotique",
+    primary:
+      "Fusil à longue portée qui soigne les alliés et inflige des dégâts aux ennemis.",
+    secondary: "Maintenez pour zoomer.",
+  },
+  "22I002": {
+    name: "Fléchette Hypodermique",
+    primary: "Lance une fléchette qui endort un ennemi.",
+  },
+  "22I003": {
+    name: "Grenade Biotique",
+    primary:
+      "Lance une grenade qui soigne, augmente les soins reçus par les alliés, et inflige des dégâts aux ennemis, qui ne peuvent plus être soignés.",
+  },
+  "22I004": {
+    name: "Nanoboost",
+    primary:
+      "Augmente les dégâts d'un allié, et réduit les dégâts qu'il subit.",
+  },
+  "14I001": {
+    name: "Pacificateur",
+    primary: "Revolver précis et puissant.",
+    secondary: "Tire frénétiquement les balles restantes.",
+  },
+  "14I002": {
+    name: "Roulade",
+    primary:
+      "Vous effectuez une roulage dans la direction que vous visez, ce qui réduit les dégâts que vous subissez et recharge votre arme.",
+    notboosted: true,
+  },
+  "14I003": {
+    name: "Grenade Flash",
+    primary:
+      "Vous entravez les adversaires devant vous, ce qui les ralentit et les empêche d'utiliser leurs capacités de déplacement.",
+  },
+  "14I004": {
+    name: "Implacable",
+    primary: "Faites face à vos ennemis. Tirez pour faire feu.",
+  },
+  "29I001": {
+    name: "Le Cobra",
+    primary: "Fusil semi-automatique.",
+    secondary:
+      "Maintenez pour zoomer, ce qui augmente les dégâts et la précision, mais ralentit la cadence de tir.",
+  },
+  "29I002": {
+    name: "Canon Scié",
+    primary:
+      "Vous tirez sur les ennemis devant vous et vous vous projetez en arrière.",
+  },
+  "29I003": {
+    name: "Dynamite",
+    primary:
+      "Vous lancez un explosif qui explose après un court instant, ou immédiatement quand vous lui tirez dessus.",
+  },
+  "29I004": {
+    name: "B.O.B.",
+    primary:
+      "Vous déployez Bob. Il charge vers l'avant et propulse ses ennemis dans les airs, avant d'attaquer avec ses canons à bras.",
+  },
+  "41I001": {
+    name: "Médiblaster",
+    primary:
+      "Arme à salves de projectiles qui soignent les unités alliées et infligent des dégâts aux adversaires.",
+  },
+  "41I002": {
+    name: "Torpilles à neutrons",
+    primary:
+      "Verrouillez puis tirez pour déclencher des projectiles à tête chercheuse qui soignent les cibles alliées sur la durée et infligent des dégâts aux adversaires.",
+  },
+  "41I003": {
+    name: "Vol Plané",
+    primary: "Plane horizontalement avec une vitesse de déplacement accrue.",
+    notboosted: true,
+  },
+  "41I004": {
+    name: "Hyperanneau",
+    primary:
+      "Déploie un annveau qui augmente la vitesse de déplacement des membres de l'équipe passant à travers.",
+    notboosted: true,
+  },
+  "41I005": {
+    name: "Rayon Orbital",
+    primary:
+      "Invoque un rayon qui se déplace vers l'avant, soigne les cibles alliées et augmente les dégâts qu'elles infligent.",
+  },
+  "41I006": {
+    name: "Bottes Martiennes",
+    primary:
+      "Depuis les airs, appuyez sur Saut pour effectuer un double saut et maintenez Saut pour planer.",
+  },
+  "20I001": {
+    name: "Canon Endothermique",
+    primary: "Arme à dispersion de courte portée qui ralentit les adversaires.",
+    secondary: "Lance-stalactites à longue portée.",
+  },
+  "20I002": {
+    name: "Cryostase",
+    primary: "Rend invulnérable et vous soigne.",
+    notboosted: true,
+  },
+  "20I003": {
+    name: "Mur de Glace",
+    primary: "Crée un mur devant vous.",
+    notboosted: true,
+  },
+  "20I004": {
+    name: "Blizzard",
+    primary:
+      "Lance un drone de contrôle de la météo qui gêle les ennemis dans une large zone.",
+  },
+  "16I001": {
+    name: "Ampli",
+    primary: "Lanceur de projectiles soniques.",
+  },
+  "16I002": {
+    name: "Crossfade",
+    primary:
+      "Alterne entre deux morceaux. Beat de soins soigne les alliés proches et Beat de vitesse augmente leur vitesse.",
+  },
+  "16I003": {
+    name: "Volume Max.",
+    primary: "Augmente l'efficacité du morceau en cours.",
+  },
+  "16I004": {
+    name: "Onde Sonore",
+    primary:
+      "Vous créez une onde de choc à courte portée pouvant repousser les ennemis.",
+  },
+  "16I005": {
+    name: "Mur de Son",
+    primary:
+      "Vous générez des points de vie supplémentaires temporaires pour les personnages alliés à proximité.",
+  },
+  "16I006": {
+    name: "Wall Ride",
+    primary: "Sautez sur un mur et maintenez pour glisser dessus.",
+  },
+  "43I001": {
+    name: "Arbalète Rafale",
+    primary: "Une arme automatique qui projette des carreaux en rafale.",
+  },
+  "43I002": {
+    name: "Dans le Mille",
+    primary:
+      "Vous ralentissez pour viser avant de tirer un carreau explosif extrêmement rapide. [Dans le mille] est réinitialisé lorsque vous utilisez [Ruée Rapide]",
+  },
+  "43I003": {
+    name: "Ruée Rapide",
+    primary:
+      "Vous bondissez dans la direction dans laquelle vous vous déplacez et [Dans le mille] est réinitialisé.",
+  },
+  "43I004": {
+    name: "Courant Ascendant",
+    primary:
+      "Vous vous projettez dans les airs grâce à une bourrasque de vent.",
+  },
+  "43I005": {
+    name: "Tir de Bolas",
+    primary:
+      "Vous lancez des bolas explosifs. Si vous touchez un personnage adverse, les bolas s'enroulent autour de la victime et attirent les adversaires à proxmité.",
+  },
+  "43I006": {
+    name: "Chasseuse de Primes",
+    primary:
+      "Tous les 3 coups de grâce, vous recevez un bonus de charge de capacité ultime.",
+  },
+};
