0d9e2db545bf6584dbf42d6b01963683d1de1d9f
GGA <gabriel.gasnot@gmail.com>
2025-06-25 10:29:10 +0200

    adding new heroes to hero selection

--- src/locales/fr/heroes.ts
+++ src/locales/fr/heroes.ts
@@ -5,11 +5,13 @@ export default {
       24, // Orisa
       34, // Junker Queen
       5, // Reinhardt
+      31, // Sigma
       13, // Zarya
 
       // DPS
       29, // Ashe
       14, // Cassidy
+      18, // Chacal
       2, // Faucheur
       43, // Freja
       21, // Genji
@@ -23,6 +25,7 @@ export default {
       35, // Kiriko
       16, // Lucio
       26, // Moira
+      10, // Zenyatta
     ],
     names: {
       "19": "D.Va",
@@ -43,5 +46,8 @@ export default {
       "6": "Ange",
       "26": "Moira",
       "43": "Freja",
+      "10": "Zenyatta",
+      "18": "Chacal",
+      "31": "Sigma",
     },
   };
