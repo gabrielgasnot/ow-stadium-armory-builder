81e186cb3c304078b44ab35be346d64e3156e8c2
Gabriel GASNOT <gabriel.gasnot@gmail.com>
2025-07-06 12:01:46 +0200

    import items with grade (#73)

--- src/services/query-items.ts
+++ src/services/query-items.ts
@@ -1,13 +1,15 @@
 import { getLocalizedItems } from "../db/db";
+import { GradedItem } from "../models/graded-item";
 import { Hero } from "../models/hero";
 import { Item } from "../models/item";
 import { AttributeType } from "../models/types/attribute-type";
 import { ItemCategory } from "../models/types/item-category";
 
-const parseItem = (item: any): Item => {
+const parseItem = (item: any, grade: string): GradedItem => {
   return {
     ...item,
     category: item.category as ItemCategory,
+    grade: grade,
     attributes: item.attributes.map((attr: any) => ({
       ...attr,
       type: attr.type as AttributeType,
@@ -17,19 +19,19 @@ const parseItem = (item: any): Item => {
 
 const getAllItems = (): Item[] => {
   const basicItems = getLocalizedItems();
-  
+
   return [
-    ...basicItems.common.map((item) => parseItem(item)),
-    ...basicItems.rare.map((item) => parseItem(item)),
-    ...basicItems.epic.map((item) => parseItem(item)),
+    ...basicItems.common.map((item) => parseItem(item, "common")),
+    ...basicItems.rare.map((item) => parseItem(item, "rare")),
+    ...basicItems.epic.map((item) => parseItem(item, "epic")),
   ];
 };
 
 const getAllItemsByHero = (hero: Hero) => {
   return [
-    ...hero?.items.common.map((item) => parseItem(item)),
-    ...hero?.items.rare.map((item) => parseItem(item)),
-    ...hero?.items.epic.map((item) => parseItem(item)),
+    ...hero?.items.common.map((item) => parseItem(item, "common")),
+    ...hero?.items.rare.map((item) => parseItem(item, "rare")),
+    ...hero?.items.epic.map((item) => parseItem(item, "epic")),
     ...getAllItems(),
   ].filter((item: Item) => !item.disabled);
 };
